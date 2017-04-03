e0480144a713d1c074700d383f18cb1dfd7d4adf
Aimee <aimee.tacchi@octoenergy.com>
2017-04-03 20:39:50 +0100

    Video 5 updates.

--- myFirstReactApp/app/components/Profile.js
+++ myFirstReactApp/app/components/Profile.js
@@ -3,8 +3,11 @@ var Router = require('react-router');
 var Repos = require('./Github/Repos');
 var UserProfile = require('./Github/UserProfile');
 var Notes = require('./Notes/Notes');
+var ReactFireMixin = require('reactfire');
+var Firebase = require('firebase');
 
 var Profile = React.createClass({
+	mixins: [ReactFireMixin],
 	getInitialState: function(){
 		return {
 			notes: [1,2,3],
@@ -14,6 +17,15 @@ var Profile = React.createClass({
 			repos: ['a', 'b', 'c']
 		}
 	},
+	componentDidMount: function(){
+		this.ref = new Firebase("https://github-note-taker-414cd.firebaseio.com/");
+		var childRef = this.ref.child(this.props.params.username);
+		this.bindAsArray(childRef, 'notes');
+				
+	},
+	componentWillUnmount: function(){
+		this.unbind('notes');		
+	},
 	render: function(){
 		return (
 		<div className="row">
@@ -21,13 +33,13 @@ var Profile = React.createClass({
 		    	<UserProfile username={this.props.params.username} bio={this.state.bio} />
 		    </div>
 		    <div className="col-md-4">
-		    	<Repos  repos={this.state.repos} />
+		    	<Repos username={this.props.params.username} repos={this.state.repos} />
 		    </div>
 		    <div className="col-md-4">
-		    	<Notes notes={this.state.notes} />
+		    	<Notes username={this.props.params.username} notes={this.state.notes} />
 		    </div>
 		</div>
-			)
+		)
 	}
 });
 
